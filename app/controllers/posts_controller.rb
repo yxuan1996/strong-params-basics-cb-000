cb646b5f69db5db873dffdc271c159d77eb46972
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-24 11:24:04 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params["post"])
+	  @post = Post.new(params.require(:post).permit(:title, :description))
 	  @post.save
 	  redirect_to post_path(@post)
 	end
